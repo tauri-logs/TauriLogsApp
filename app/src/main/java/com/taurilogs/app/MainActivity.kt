605207daace0a7b012fe77b456c1f3e2ee8bd52b
Jakub Jukl <jukljakub@gmail.com>
2022-10-06 15:10:40 +0200

    added toolbar

--- app/src/main/java/com/taurilogs/app/MainActivity.kt
+++ app/src/main/java/com/taurilogs/app/MainActivity.kt
@@ -2,10 +2,34 @@ package com.taurilogs.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import com.taurilogs.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        toolbar = binding.toolbar
+        toolbar.title = "Tauri Logs"
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        println("aaaaaa")
+        return true
     }
 }
